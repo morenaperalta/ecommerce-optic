2bf014dbbdf2dba51327cf098783d077009b140d
J-uds <206530139+J-uds@users.noreply.github.com>
2025-06-25 14:21:12 +0200

    build: initialize Spring Boot project structure

---
+++ src/main/java/com/irisandco/ecommerce_optic/EcommerceOpticApplication.java
@@ -0,0 +1,13 @@
+package com.irisandco.ecommerce_optic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceOpticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceOpticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irisandco/ecommerce_optic/EcommerceOpticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irisandco.ecommerce_optic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceOpticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
