b38c294cf9c6f171f5b4555c5af976725fa96bf8
More-Pe <peraltamorena.va@gmail.com>
2025-06-26 11:54:37 +0200

    feat(UserRequest): add attributes

---
+++ src/main/java/com/irisandco/ecommerce_optic/user/UserRequest.java
@@ -0,0 +1,20 @@
+package com.irisandco.ecommerce_optic.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRequest (
+        @NotBlank
+        @Size(min = 2, max = 50, message = "User must contain min 2 and max 50 characters")
+        String username,
+        @NotBlank
+        @Size(min = 2, max = 50, message = "Email must contain min 2 and max 50 characters")
+        String email,
+        @NotBlank
+        @Size(min = 12, max = 50, message = "Password must contain min 12 and max 50 characters")
+        String password
+
+)
+{
+
+}
