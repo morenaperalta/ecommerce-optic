b4dc62cb7c43a5bfbbcb2fbe698aeb65a8dae854
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-06-26 10:51:27 +0200

    feat(CategoryMapper): add methods to CategoryMapper

---
+++ src/main/java/com/irisandco/ecommerce_optic/category/CategoryMapper.java
@@ -0,0 +1,23 @@
+package com.irisandco.ecommerce_optic.category;
+
+import java.util.List;
+
+public class CategoryMapper {
+    public static Category toEntity(CategoryRequest categoryRequest){
+        return new Category(categoryRequest.name());
+    }
+
+//    public static Category toEntity(CategoryRequest categoryRequest){
+//        Category category = new Category();
+//        category.setName(categoryRequest.name());
+//        return category;
+//    }
+
+    public static CategoryResponse toDto(Category category, List<ProductResponseShort> products){
+        return new CategoryResponse(category.getId(), category.getName(), products);
+    }
+
+    public static CategoryResponseShort toDtoShort(Category category){
+        return new CategoryResponseShort(category.getId(), category.getName());
+    }
+}
