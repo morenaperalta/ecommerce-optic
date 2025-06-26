d4445b836a335d0569a3038710e7b19089e017e7
J-uds <206530139+J-uds@users.noreply.github.com>
2025-06-26 12:38:53 +0200

    feat(Product): add attributes, getters, setters and constructors

---
+++ src/main/java/com/irisandco/ecommerce_optic/product/Product.java
@@ -0,0 +1,103 @@
+package com.irisandco.ecommerce_optic.product;
+
+import com.irisandco.ecommerce_optic.category.Category;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id_product")
+    private Long  id;
+
+    @Column(name="name", table="products", insertable=true, updatable=true, nullable=false, columnDefinition = "VARCHAR(50) NOT NULL UNIQUE")
+    private String name;
+
+    @Column(name="price", table="products", insertable=true, updatable=true, nullable=true, columnDefinition = "DOUBLE")
+    private Double price;
+
+    @Column(name="imageUrl", table="products", insertable=true, updatable=true, nullable=true, columnDefinition = "VARCHAR(50)")
+    private String imageUrl;
+
+    @Column(name="featured", table="products", insertable=true, updatable=true, nullable=true, columnDefinition = "BOOLEAN")
+    private Boolean featured;
+
+    @ManyToMany
+    @JoinTable(
+            name="category_product",
+            joinColumns = @JoinColumn(name="id_product"),
+            inverseJoinColumns = @JoinColumn(name="id_category"))
+    private List<Category> categories;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, Double price, String imageUrl, Boolean featured, List<Category> categories) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.featured = featured;
+        this.categories = categories;
+    }
+
+    public Product(String name, Double price, String imageUrl, Boolean featured, List<Category> categories) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.featured = featured;
+        this.categories = categories;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Boolean getFeatured() {
+        return featured;
+    }
+
+    public void setFeatured(Boolean featured) {
+        this.featured = featured;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+}
