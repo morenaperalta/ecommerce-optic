5f1ee874f4846974acea3631cc307628d94949ca
J-uds <206530139+J-uds@users.noreply.github.com>
2025-06-26 14:16:00 +0200

    feat(PreductResponse): add attributes

--- src/main/java/com/irisandco/ecommerce_optic/product/ProductResponse.java
+++ src/main/java/com/irisandco/ecommerce_optic/product/ProductResponse.java
@@ -1,6 +1,15 @@
 package com.irisandco.ecommerce_optic.product;
 
-public record ProductResponse(
+import com.irisandco.ecommerce_optic.category.CategoryResponseShort;
 
+import java.util.List;
+
+public record ProductResponse(
+    Long id,
+    String name,
+    Double price,
+    String imageUrl,
+    Boolean featured,
+    List<CategoryResponseShort> categories
 ) {
 }
