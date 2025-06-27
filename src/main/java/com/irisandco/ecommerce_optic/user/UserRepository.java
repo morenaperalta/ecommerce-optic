bf07b77afd149e58fc26d4a51c62eff3943f7609
More-Pe <peraltamorena.va@gmail.com>
2025-06-27 12:58:45 +0200

    feat(UserRepository): add custom methods

--- src/main/java/com/irisandco/ecommerce_optic/user/UserRepository.java
+++ src/main/java/com/irisandco/ecommerce_optic/user/UserRepository.java
@@ -3,6 +3,10 @@ package com.irisandco.ecommerce_optic.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository <User, Long> {
+    Optional<User> findUserByUsername(String username);
+    Boolean existByUsername(String username);
 }
