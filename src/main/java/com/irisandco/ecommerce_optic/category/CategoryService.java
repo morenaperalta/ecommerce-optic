dc898fb42bb94b5b4752abb21282751a9009c5c4
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-06-27 21:16:42 +0200

    feat(CategoryService): add methods

---
+++ src/main/java/com/irisandco/ecommerce_optic/category/CategoryService.java
@@ -0,0 +1,58 @@
+package com.irisandco.ecommerce_optic.category;
+
+import com.irisandco.ecommerce_optic.product.ProductMapper;
+import com.irisandco.ecommerce_optic.product.ProductResponseShort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository CATEGORY_REPOSITORY;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        CATEGORY_REPOSITORY = categoryRepository;
+    }
+
+    public List<CategoryResponse> getAll(){
+        return listToDto(CATEGORY_REPOSITORY.findAll());
+    }
+
+    public Category getById(Long id){
+        return CATEGORY_REPOSITORY.findById(id).orElseThrow(() -> new IllegalArgumentException("Category not found"));
+
+    }
+
+    public CategoryResponseShort saveCategory(CategoryRequest categoryRequest){
+        Category category = CategoryMapper.toEntity(categoryRequest);
+        return CategoryMapper.toDtoShort(CATEGORY_REPOSITORY.save(category));
+    }
+
+    public CategoryResponseShort updateCategory(Long id, CategoryRequest categoryRequest){
+        Category category = getById(id);
+        category.setName(categoryRequest.name());
+        return CategoryMapper.toDtoShort(CATEGORY_REPOSITORY.save(category));
+    }
+
+    public void deleteCategory(Long id){
+        Category category = getById(id);
+        CATEGORY_REPOSITORY.deleteById(id);
+    }
+
+    private List<CategoryResponseShort> listToDtoShort(List<Category> categories) {
+        return categories.stream()
+                .map(CategoryMapper::toDtoShort)
+                .toList();
+    }
+
+    private List<CategoryResponse> listToDto(List<Category> categories) {
+        return categories.stream()
+                .map( (category) -> {
+                                List <ProductResponseShort> productResponseShort = category.getProducts().stream()
+                                                    .map(ProductMapper::toDtoShort)
+                                                    .toList();
+                                return CategoryMapper.toDto(category, productResponseShort);
+                })
+                .toList();
+    }
+}
