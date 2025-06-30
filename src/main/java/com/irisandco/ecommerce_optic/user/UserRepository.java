0a2e30b383b0b1c2198889cb96916b5bd761cc79
More-Pe <peraltamorena.va@gmail.com>
2025-06-30 14:07:50 +0200

    fix(UserRepository): change name of method

--- src/main/java/com/irisandco/ecommerce_optic/user/UserRepository.java
+++ src/main/java/com/irisandco/ecommerce_optic/user/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository <User, Long> {
     Optional<User> findUserByUsername(String username);
-    Boolean existByUsername(String username);
+    Boolean existsByUsername(String username);
 }
