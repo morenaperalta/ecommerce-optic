6503a1ea56a361683a882abdc98eb14dd733300d
J-uds <206530139+J-uds@users.noreply.github.com>
2025-06-30 14:31:32 +0200

    feat(ProductRepository): add customized methods

--- src/main/java/com/irisandco/ecommerce_optic/product/ProductRepository.java
+++ src/main/java/com/irisandco/ecommerce_optic/product/ProductRepository.java
@@ -3,6 +3,10 @@ package com.irisandco.ecommerce_optic.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findProductByName(String name);
+    boolean existsByName(String name);
 }
