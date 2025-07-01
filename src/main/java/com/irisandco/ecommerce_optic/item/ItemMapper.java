a82358ac384b3112bf50e1fb272f463c6571266d
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-07-01 13:12:00 +0200

    fix(ItemMapper): delete ItemRequest and update toDto method

--- src/main/java/com/irisandco/ecommerce_optic/item/ItemMapper.java
+++ src/main/java/com/irisandco/ecommerce_optic/item/ItemMapper.java
@@ -3,11 +3,9 @@ package com.irisandco.ecommerce_optic.item;
 import com.irisandco.ecommerce_optic.product.Product;
 
 public class ItemMapper {
-    public static Item toEntity(ItemRequest itemRequest, Product product, Cart cart){
-        return new Item(itemRequest.quantity(), product, cart);
-    }
 
-    public static ItemResponse toDto(Item item, Product product){
+    public static ItemResponse toDto(Item item){
+        Product product = item.getProduct();
         return new ItemResponse(item.getId(), product.getId(), product.getName(), item.getQuantity(), product.getPrice());
     }
 }
