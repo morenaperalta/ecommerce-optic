d18d9b81a182a1a5cf7b8398072cf36b70e39e74
More-Pe <peraltamorena.va@gmail.com>
2025-07-01 14:19:12 +0200

    feat(ErrorResponse): add attributes, constructor and getters

--- src/main/java/com/irisandco/ecommerce_optic/exception/ErrorResponse.java
+++ src/main/java/com/irisandco/ecommerce_optic/exception/ErrorResponse.java
@@ -1,4 +1,42 @@
 package com.irisandco.ecommerce_optic.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
 public class ErrorResponse {
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime timestamp;
+    private final int status;
+    private final String error;
+    private final String message;
+    private final String path;
+
+    public ErrorResponse(int status, String error, String message, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getPath() {
+        return path;
+    }
 }
