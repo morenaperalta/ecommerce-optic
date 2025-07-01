b93167b62abf9a364a2311d93a4f43336f95e540
J-uds <206530139+J-uds@users.noreply.github.com>
2025-07-01 15:08:34 +0200

    feat(GlobalExceptionHandler): add handleEntityNotFoundException method

--- src/main/java/com/irisandco/ecommerce_optic/exception/GlobalExceptionHandler.java
+++ src/main/java/com/irisandco/ecommerce_optic/exception/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.irisandco.ecommerce_optic.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException exception) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        ErrorResponse errorResponse = new ErrorResponse(status, exception.getMessage(), Arrays.toString(exception.getStackTrace()));
+
+        return new ResponseEntity<>(errorResponse,status);
+    }
 }
