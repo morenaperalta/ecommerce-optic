878ed319e48e34ebf943c83b934269d41ff9fc4d
J-uds <206530139+J-uds@users.noreply.github.com>
2025-07-03 16:49:07 +0200

    tes(product): add setup

--- src/test/java/com/irisandco/ecommerce_optic/product/ProductServiceTest.java
+++ src/test/java/com/irisandco/ecommerce_optic/product/ProductServiceTest.java
@@ -1,4 +1,50 @@
 package com.irisandco.ecommerce_optic.product;
 
+import com.irisandco.ecommerce_optic.category.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+//Activa Mockito en JUnit 5
+@ExtendWith((MockitoExtension.class))
 public class ProductServiceTest {
+
+    //Creo un falso repositorio, que no se conecta a la base de datos.
+    @Mock
+    private ProductRepository productRepository;
+
+    //Creo un servicio de categorías falso. Se usa cuando el ProductService necesita cargar categorías.
+    @Mock
+    private CategoryService categoryService;
+
+    //Crea un ProductService de verdad, pero le inyecta los dos mocks anteriores.
+    @InjectMocks
+    private ProductService productService;
+
+    private Product productEntity;
+    private ProductResponse productResponse;
+    private ProductRequest productRequest;
+    private Product productMapper;
+
+    @BeforeEach
+    void setUp() {
+        productEntity = new Product(1L,"Sunglasses", 100.0, "image.jpg", true, List.of());
+        productResponse = new ProductResponse(1L, "Sunglasses", 100.0, "image.jpg", true, List.of());
+        productRequest = new ProductRequest("Sunglasses", 100.0, "image.jpg", true, List.of());
+        productMapper =new Product("Sunglasses", 100.0, "image.jpg", true, List.of());
+    }
+
+    @Test
+    void getAllProducts_whenProductExist_returnListOfProductResponse() {
+
+
+
+    }
+
+
 }
