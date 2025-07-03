5b25de906d18e73a73a294e6da10ee3954b0b67d
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-07-03 11:41:47 +0200

    feat(CategoryServiceTest): add getAllCategories test

--- src/test/java/com/irisandco/ecommerce_optic/category/CategoryServiceTest.java
+++ src/test/java/com/irisandco/ecommerce_optic/category/CategoryServiceTest.java
@@ -1,4 +1,40 @@
 package com.irisandco.ecommerce_optic.category;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CategoryServiceTest {
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    private CategoryService categoryService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        categoryService = new CategoryService(categoryRepository);
+    }
+
+    @Test
+    void getAllCategories_returnsListOfCategoriesResponse() {
+        // Mock de categoría
+        Category categoryEntity = new Category("Category 1");
+
+        when(categoryRepository.findAll()).thenReturn(List.of(categoryEntity));
+
+        // Llamada al método real
+        List<CategoryResponse> result = categoryService.getAllCategories();
+
+        // Asserts básicos
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("Category 1", result.get(0).name());
+    }
 }
