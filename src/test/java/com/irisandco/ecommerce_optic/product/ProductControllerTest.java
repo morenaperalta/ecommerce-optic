a41300177ae1e32cce79e980f281b3472d1642c5
J-uds <206530139+J-uds@users.noreply.github.com>
2025-07-03 15:48:20 +0200

    change location of banner.txt to resources

--- src/test/java/com/irisandco/ecommerce_optic/product/ProductControllerTest.java
+++ src/test/java/com/irisandco/ecommerce_optic/product/ProductControllerTest.java
@@ -1,4 +1,41 @@
 package com.irisandco.ecommerce_optic.product;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+
+@WebMvcTest(ProductControllerTest.class)
 public class ProductControllerTest {
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productServiceMock;
+
+    @BeforeEach
+    //Antes de cada test prepara esto (setup)
+    public void setUp() {
+        //Creo un objeto falso que imita el servicio. No ejecuta lógica real.
+        productServiceMock = Mockito.mock(ProductService.class);
+
+        //Creo controlador real manualmente, pero le doy el servicio falso con el mock.
+        ProductController productController = new ProductController(productServiceMock);
+
+        //Creo manualmente el entorno de test para poder simular peticiones HTTP
+        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+    }
+
+    /*@Test
+    void getAllProducts_returnList() throws Exception {
+        ProductResponse product1 = new ProductResponse("Product 1", List.of("subproducts"));
+
+        Mockito.when(ProductService.getAllProducts()).thenReturn(List.of(product1));
+    }*/
 }
--- src/test/java/com/irisandco/ecommerce_optic/product/ProductServiceTest.java
+++ src/test/java/com/irisandco/ecommerce_optic/product/ProductServiceTest.java
@@ -1,4 +1,7 @@
 package com.irisandco.ecommerce_optic.product;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(ProductServiceTest.class)
 public class ProductServiceTest {
 }
