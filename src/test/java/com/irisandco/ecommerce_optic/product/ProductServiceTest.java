ca791ec489fe40f7b87a4947d9fc1391eb5ead6c
J-uds <206530139+J-uds@users.noreply.github.com>
2025-07-04 15:11:49 +0200

    test(product): add findAll, findById test methods

--- src/test/java/com/irisandco/ecommerce_optic/product/ProductServiceTest.java
+++ src/test/java/com/irisandco/ecommerce_optic/product/ProductServiceTest.java
@@ -9,9 +9,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 //Activa Mockito en JUnit 5
-@ExtendWith((MockitoExtension.class))
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
     //Creo un falso repositorio, que no se conecta a la base de datos.
@@ -42,8 +46,39 @@ public class ProductServiceTest {
     @Test
     void getAllProducts_whenProductExist_returnListOfProductResponse() {
 
+        when(productRepository.findAll()).thenReturn(List.of(productEntity));
 
+        List<ProductResponse> result = productService.getAllProducts();
 
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("Sunglasses", result.getFirst().name());
+        assertEquals(100.0, result.getFirst().price());
+        assertEquals("image.jpg", result.getFirst().imageUrl());
+        assertEquals(true, result.getFirst().featured());
+        assertEquals(List.of(), result.getFirst().categories());
+
+        verify(productRepository, times(1)).findAll();
+
+    }
+
+    @Test
+    void grtProductById_whenUserExists_returnUserEntity() {
+
+        Long id = 1L;
+        when(productRepository.findById(eq(id))).thenReturn(Optional.of(productEntity));
+
+        Product result = productService.getProductById(id);
+
+        assertNotNull(result);
+        assertEquals(Product.class, result.getClass());
+        assertEquals("Sunglasses", result.getName());
+        assertEquals(100.0, result.getPrice());
+        assertEquals("image.jpg", result.getImageUrl());
+        assertEquals(true, result.getFeatured());
+        assertEquals(List.of(), result.getCategories());
+
+        verify(productRepository, times(1)).findById(id);
     }
 
 
