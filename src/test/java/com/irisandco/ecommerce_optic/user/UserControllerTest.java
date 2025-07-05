055d577cc381bf3504119be67f9f7e1ef52d046b
More-Pe <peraltamorena.va@gmail.com>
2025-07-05 14:41:40 +0200

    feat(UserControllerTest): add shouldGetAllUsersSuccessfully test

--- src/test/java/com/irisandco/ecommerce_optic/user/UserControllerTest.java
+++ src/test/java/com/irisandco/ecommerce_optic/user/UserControllerTest.java
@@ -1,4 +1,63 @@
 package com.irisandco.ecommerce_optic.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private List<UserResponse> userResponses;
+
+    @BeforeEach
+    void setUp() {
+       userResponses = new ArrayList<>();
+
+       UserResponse user1 = new UserResponse(1L, "john_doe", "user1@gmail.com");
+       UserResponse user2 = new UserResponse(2L, "jane_doe", "user2@hotmail.com");
+
+        userResponses.add(user1);
+        userResponses.add(user2);
+    }
+
+    @Test
+    void shouldGetAllUsersSuccessfully() throws Exception{
+        given(userService.getAllUsers()).willReturn(userResponses);
+
+        mockMvc.perform(get("/api/users").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[0].username").value("john_doe"))
+                .andExpect(jsonPath("$[1].username").value("jane_doe"))
+                .andExpect(jsonPath("$[0].email").value("user1@gmail.com"))
+                .andExpect(jsonPath("$[1].email").value("user2@hotmail.com"));
+
+    }
+
 }
