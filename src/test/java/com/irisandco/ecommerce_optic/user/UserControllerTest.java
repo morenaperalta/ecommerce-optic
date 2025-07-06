9ee5f44d43849d3e81bbcdbf4b1a9a87a24e0a16
More-Pe <peraltamorena.va@gmail.com>
2025-07-06 14:01:44 +0200

    feat(UserControllerTest): refactor users and add getUserById_whenUserExists_returnsUserResponse test

--- src/test/java/com/irisandco/ecommerce_optic/user/UserControllerTest.java
+++ src/test/java/com/irisandco/ecommerce_optic/user/UserControllerTest.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -31,17 +33,17 @@ public class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private UserResponse user1;
+    private UserResponse user2;
     private List<UserResponse> userResponses;
 
     @BeforeEach
     void setUp() {
        userResponses = new ArrayList<>();
-
-       UserResponse user1 = new UserResponse(1L, "john_doe", "user1@gmail.com");
-       UserResponse user2 = new UserResponse(2L, "jane_doe", "user2@hotmail.com");
-
-        userResponses.add(user1);
-        userResponses.add(user2);
+       user1 = new UserResponse(1L, "Judit", "judit@gmail.com");
+       user2 = new UserResponse(2L, "Iris", "iris@hotmail.com");
+       userResponses.add(user1);
+       userResponses.add(user2);
     }
 
     @Test
@@ -53,11 +55,24 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$", Matchers.hasSize(2)))
                 .andExpect(jsonPath("$[0].id").value(1))
                 .andExpect(jsonPath("$[1].id").value(2))
-                .andExpect(jsonPath("$[0].username").value("john_doe"))
-                .andExpect(jsonPath("$[1].username").value("jane_doe"))
-                .andExpect(jsonPath("$[0].email").value("user1@gmail.com"))
-                .andExpect(jsonPath("$[1].email").value("user2@hotmail.com"));
+                .andExpect(jsonPath("$[0].username").value("Judit"))
+                .andExpect(jsonPath("$[1].username").value("Iris"))
+                .andExpect(jsonPath("$[0].email").value("judit@gmail.com"))
+                .andExpect(jsonPath("$[1].email").value("iris@hotmail.com"));
 
     }
+
+    @Test
+    void getUserById_whenUserExists_returnsUserResponse() throws Exception{
+        Long userId = 1L;
+        given(userService.getUserResponseById(userId)).willReturn(user1);
+
+        mockMvc.perform(get("/api/users/{id}", userId).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.username").value("Judit"))
+                .andExpect(jsonPath("$.email").value("judit@gmail.com"));
+    }
+
 
 }
