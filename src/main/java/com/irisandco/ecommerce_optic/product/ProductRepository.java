8c6328794e2422e85ebd899e67b4029f0b6ce590
J-uds <206530139+J-uds@users.noreply.github.com>
2025-07-07 15:19:49 +0200

    Update ProductRepository.java
    remove comment

--- src/main/java/com/irisandco/ecommerce_optic/product/ProductRepository.java
+++ src/main/java/com/irisandco/ecommerce_optic/product/ProductRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findProductByNameIgnoreCase(String name);
     boolean existsByNameIgnoreCase(String name);
-    //Para filtrar
+   
     List<Product> findByCategories_NameIgnoreCase(String categoryName);
     List<Product> findByPriceLessThanEqual(Double maxPrice);
     List<Product> findByPriceGreaterThanEqual(Double minPrice);
